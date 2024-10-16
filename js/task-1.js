212ce746f9021638e4fdc00c8166276f791e1b72
cryptmask <emreozkurt1978@gmail.com>
2024-10-16 22:59:08 +0300

    final commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,11 @@
+function slugify(title) {
+  return title.toLowerCase().split(" ").join("-");
+}
+
+const title = "";
+const slug = slugify(title);
+let newLine = "\n ";
+
 console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
